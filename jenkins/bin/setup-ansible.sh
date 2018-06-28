65b4b5f4e514443f9c25dd01a5256cc63d96092d
dareolu <dare.david.olufunmilayo@gmail.com>
2018-06-28 16:50:51 +0100

    removed redundant directories

--- jenkins/bin/setup-ansible.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# Install Ansible
-sudo bash -c 'echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list'
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt-get update
-sudo apt-get install ansible -y
